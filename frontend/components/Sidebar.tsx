a0d3db25053522946fec033c551aa5f7a6788e6e
Phineas-Tracer <antonioguerrerocedron@gmail.com>
2026-06-24 21:47:24 +0000

    ekfeejfjfiejf

---
+++ frontend/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { link } from "react-router-dom";
+
+interface SidebarProps {
+    isOpen: boolean;
+    onLogout: () => void;
+}
+
+export default function Sidebar({ isOpen, onLogout }: SidebarProps) {
+    if (!isOpen) return null;
+    return(
+        <div className="w-64 bg-gray-100 h-full p-4 shadow-lg">
+            <nav className="flex flex-col gap-4">
+                <Link to="/search">
+                    Search Flights
+                </Link>
+
+                <Link to="/bookings">
+                    My Booked Flights
+                </Link>
+
+                <button 
+                onClick={onLogout}
+                className="text-red-600 text-left"
+                >
+                    Logout
+                </button>
+            </nav>
+        </div>
+    )
+}
